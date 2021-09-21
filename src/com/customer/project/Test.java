96bb2484df3145901e99fada5b7509d9c1d79646
mahmutcandurak <mahmutdurak69@gmail.com>
2021-09-21 15:48:17 +0300

    Comment lines updated

--- src/com/customer/project/Test.java
+++ src/com/customer/project/Test.java
@@ -13,23 +13,18 @@ import java.util.Scanner;
 
 public class Test implements Taxable {
 
-
     public static void main(String[] args) {
-
-
 
         Scanner scanner = new Scanner(System.in);
 
-        Customer customer1 = new Customer("istanbul", "Can Yılmaz", "5051234567", 1);
-        Customer customer2 = new Customer("Ankara", "Selim Avcı", "5059876543", 2);
-
+        //Urunlerin isimlerinin, fiyatlarinin ve turune bagli diger ozelliklerinin product tipinde nesnelerde tutulmasi.
         Product product1 = new Hardware("monitor", 11, "LG UltraWide", 1500.00, 2);
         Product product2 = new Manuel("production", 12, "ManuelPro", 120.00,"admin");
         Product product3 = new Software("Win10", 13, "Windows 10 Pro KEY", 50.00,"A1S2-F1H2-K21U-21DA");
         Product product4 = new Hardware("24 inch tv", 14, "LG 24TL510S", 700.00, 2);
         Product product5 = new Software("steam random key",15,"steamKEY",10.00,"K12K-SF3G-SDFS-SD12");
 
-
+        //Product nesnelerinde tanimlanan urunlerin stok miktarlari.
         OrderItem orderItem1 = new OrderItem(1,product1,2);
         OrderItem orderItem2 = new OrderItem(2,product2,4);
         OrderItem orderItem3 = new OrderItem(3,product3,12);
@@ -37,14 +32,15 @@ public class Test implements Taxable {
         OrderItem orderItem5 = new OrderItem(5,product5,27);
 
 
-        List<Product> productList = new ArrayList<>(Arrays.asList(product1,product2,product3,product4,product5));
-        List<String> selectedProductList = new ArrayList<>();
-        List<Double> selectedPriceList = new ArrayList<>();
+        List<Product> productList = new ArrayList<>(Arrays.asList(product1,product2,product3,product4,product5));       //Urunlerin tutuldugu liste
+        List<String> selectedProductList = new ArrayList<>();                       //Secilen urunlerin tutuldugu liste
+        List<Double> selectedPriceList = new ArrayList<>();                         //Secilen urunlerin fiyatlarinin tutuldugu liste
 
-
+        //Sirket kullanicilarinin bilgilerinin nesne olarak tutulmasi.
         Customer company1 = new Company("Ataşehir","Renova","2161112233",1);
         Customer company2 = new Company("REMOTE","Yazılım Köyü","5151232343",2);
 
+        //Bireysel kullancilarin bilgilerinin nesne olarak tutulmasi.
         Customer individual1 = new Individual("Beşiktaş","Selim Kiraz","123456578",1,"112233");
         Customer individual2 = new Individual("Üsküdar","Ceren Aygün","3256845218",2,"556677");
 
@@ -55,18 +51,18 @@ public class Test implements Taxable {
 
         int userTypeChoise = scanner.nextInt();
         int userTypeChosen = 0;
-        String strChosen = null;
 
+
+        //İslemlerin sirket olarak mi yoksa bireysel olarak mi yapılacagini sorgulayan if bloklari.
+        //Tercihe gore hangi sirket ya da hangi kullanici oldugu bilgisini de sorguluyoruz.
         if(userTypeChoise == 1) {
             System.out.println("Choose your company: 1- " + company1.getName() + " 2: " + company2.getName());
             userTypeChosen=scanner.nextInt();
             if (userTypeChosen == 1){
                 System.out.println(company1.getName()+" chosen.");
-                strChosen = company1.getName();
             }
             else if (userTypeChosen == 2) {
                 System.out.println(company2.getName()+" chosen.");
-                strChosen = company2.getName();
             }
         }
         if (userTypeChoise == 2) {
@@ -74,11 +70,9 @@ public class Test implements Taxable {
             userTypeChosen=scanner.nextInt();
             if (userTypeChosen == 1){
                 System.out.println(individual1.getName()+" chosen.");
-                strChosen = individual1.getName();
             }
             else if (userTypeChosen == 2) {
                 System.out.println(individual2.getName()+" chosen.");
-                strChosen = individual2.getName();
             }
         }
 
@@ -87,6 +81,7 @@ public class Test implements Taxable {
 
         System.out.println("Please choose your product:");
 
+        //Urunleri tuttugumuz listeyi donduren foreach blogu
         for (Product x : productList) {
             System.out.println((productList.indexOf(x)+1) + " " +x.getName() + " Price: " + x.getRetailPrice());
         }
@@ -95,8 +90,8 @@ public class Test implements Taxable {
         String productChosen = null;
         Double productPrice = null;
 
-
-
+        //Kullanici 0 girene kadar tekrar eden, urun sectirmek icin kullanilan while loop.
+        //Loop icinde switch case yapısı ile kullanicinin ürünleri secmesi saglanir. Sectigi urunler ilgili listlere atanir.
         while (productChoice != 0) {
             switch (productChoice) {
                 case 1:
@@ -167,23 +162,24 @@ public class Test implements Taxable {
                     break;
                 default:
                     System.out.println("Wrong choice. Please give correct number.");
+                    productChoice = scanner.nextInt();
+                    break;
             }
         }
 
-
-
-        List<String> orderList = new ArrayList(Arrays.asList(productChosen,strChosen));
-
+        //Kullanicinin sectigi urunleri ekrana cikartan for loop.
         System.out.println("Selected products: ");
         for (String s : selectedProductList) {
-            System.out.println(s + " ");
+            System.out.println(" - "+s + " ");
         }
 
+        //Kullanicinin sectigi urunlerin toplam fiyatini toplayan for loop.
         double total = 0;
         for (Double z : selectedPriceList) {
             total = total + z;
         }
 
+        //Toplam tutarı ekran basan komut.
         System.out.println("Total price: " + total +" $");
 
     }
